36ca79f6427f9963fa27cf2517a47331cd0f8574
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-09-29 15:10:26 +0200

    Task 1+2 done

---
+++ TicketClassLibrary/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace TicketClassLibrary
+{
+    /// <summary>
+    /// A representation of cars needing to be ticketed.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// The unique license plate, used for identifying the vehicle
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// The date at which the vehicle has crossed, given as a DateTime object.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Calculates the price for this vehicle's crossing.
+        /// </summary>
+        /// <returns>The crossing's price, in dkk</returns>
+        public double Price()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// Returns the type of vehicle, for categorization
+        /// </summary>
+        /// <returns>A string signifying the type of vehicle</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ TicketClassLibraryTests/CarTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    [ExcludeFromCodeCoverage]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest_Valid_240()
+        {
+            //arrange
+            Car c1 = new Car();
+            //act
+            double c1price = c1.Price();
+            //assert
+            Assert.AreEqual(c1price, 240);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest_Valid_Car()
+        {
+            //arrange
+            Car c2 = new Car();
+            //act
+            string c2type = c2.VehicleType();
+            //assert
+            Assert.AreEqual(c2type,"Car");
+        }
+    }
+}
