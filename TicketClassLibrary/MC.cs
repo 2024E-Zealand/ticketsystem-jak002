8eb5b1163b39282e450ae4ebb2cb54e61a8c1b9d
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-09-29 15:19:46 +0200

    Task 3 done

---
+++ TicketClassLibrary/MC.cs
@@ -0,0 +1,35 @@
+﻿namespace TicketClassLibrary
+{
+    /// <summary>
+    /// A representation of motorcycles needing to be ticketed.
+    /// </summary>
+    public class MC
+    {
+        /// <summary>
+        /// The unique license plate, used for identifying the vehicle
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// The date at which the vehicle has crossed, given as a DateTime object.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Calculates the price for this vehicle's crossing.
+        /// </summary>
+        /// <returns>The crossing's price, in dkk</returns>
+        public double Price()
+        {
+            return 125;
+        }
+        /// <summary>
+        /// Returns the type of vehicle, for categorization
+        /// </summary>
+        /// <returns>A string signifying the type of vehicle</returns>
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ TicketClassLibraryTests/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest_Valid_125()
+        {
+            //arrange
+            MC m1 = new MC();
+            //act
+            double m1price = m1.Price();
+            //assert
+            Assert.AreEqual(m1price, 125);
+        }
+
+        [TestMethod()]
+        public void VehicleTest_Valid_MC()
+        {
+            //arrange
+            MC m2 = new MC();
+            //act
+            string m2vehicle = m2.Vehicle();
+            //assert
+            Assert.AreEqual(m2vehicle, "MC");
+        }
+    }
+}
