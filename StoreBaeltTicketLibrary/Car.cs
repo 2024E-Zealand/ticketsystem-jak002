f87192a16ed7204a22ca7c89db841d6a2c5da9d8
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-09-29 18:32:47 +0200

    task 8 and 9 done

---
+++ StoreBaeltTicketLibrary/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace StoreBaeltTicketLibrary
+{
+    /// <summary>
+    /// A representation of cars needing to be ticketed, customized for Storebælt
+    /// </summary>
+    public class Car : TicketClassLibrary.Car
+    {
+        /// <summary>
+        /// Calculates the price for this vehicle's crossing, taking weekend discounts into account.
+        /// </summary>
+        /// <returns>The crossing's price, in dkk</returns>
+        public override double Price()
+        {
+            if (Date.DayOfWeek == DayOfWeek.Sunday || Date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                double price = 240 * 0.8;
+                return Brobizz ? price * 0.95 : price;
+            }
+            return base.Price();
+        }
+    }
+}
---
+++ StoreBaeltTicketLibraryTests1/CarTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [TestClass()]
+    [ExcludeFromCodeCoverage]
+    public class CarTests
+    {
+        [DataTestMethod()]
+        [DataRow(30, 240, false)]
+        [DataRow(30, 228, true)]
+        [DataRow(29,192,false)]
+        [DataRow(29,182.4,true)]
+        public void PriceTest_Valid_ExpectedPrices(int date,double expectedPrice, bool brobizz)
+        {
+            //arrange
+            Car car = new Car();
+            car.Date = new DateTime(2024, 09, date);
+            car.Brobizz = brobizz;
+            //act
+            double actualPrice = car.Price();
+            //assert
+            Assert.AreEqual(expectedPrice, actualPrice,0.01);
+        }
+    }
+}
